efedfd9df65deb6a6acc13b5998300aec7375daf
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2022-02-11 13:23:19 +0800

    GET каждого комментария поста Добавлена проверка на наличие уже сохранённого файла

--- vk_engine/vk_search.py
+++ vk_engine/vk_search.py
@@ -12,6 +12,44 @@ url = f"https://api.vk.com/method/wall.get?domain={group_name}&count=10&access_t
 req = requests.get(url)
 print(req.text)
 
+def get_comments_post(fresh_posts_id, posts, group_name):
+    print("GET COMMENTS POST")
+    posts_id = []
+    for comment in fresh_posts_id:
+        print("ПРОВЕРКА", comment)
+        url = f"https://api.vk.com/method/wall.getComments?domain={comment}&count=10&access_token={token}&v=5.131"
+        req = requests.get(url)
+        src = req.json()
+
+        # проверяем существует ли директория с именем группы
+        if os.path.exists(f"{comment}"):
+            print(f"Директория с именем {comment} уже существует!")
+        else:
+            os.mkdir(str(comment))
+
+        # сохраняем данные в json файл
+        with open(f"{comment}/{comment}.json", "w", encoding="utf-8") as file:
+            json.dump(src, file, indent=4, ensure_ascii=False)
+
+        # собираем ID новых постов
+        fresh_comments_id = []
+
+        for fresh_comment_id in posts:
+            fresh_comment_id = fresh_comment_id["id"]
+            fresh_comments_id.append(fresh_comment_id)
+
+        if not os.path.exists(f"{comment}/exist_comments_{comment}.txt"):
+            print("Файла с ID комментариев не существует, создаём файл!")
+
+            with open(f"{comment}/exist_comments_{comment}.txt", "w") as file:
+                for comment in fresh_comments_id:
+                    file.write(str(comment) + "\n")
+
+        else:
+            print("Файл с ID комментарием найден, начинаем выборку комментариев")
+
+
+
 def get_wall_posts(group_name):
     url = f"https://api.vk.com/method/wall.get?domain={group_name}&count=10&access_token={token}&v=5.131"
     req = requests.get(url)
@@ -22,7 +60,6 @@ def get_wall_posts(group_name):
         print(f"Директория с именем {group_name} уже существует!")
     else:
         os.mkdir(group_name)
-
     #сохраняем данные в json файл
     with open(f"{group_name}/{group_name}.json", "w", encoding="utf-8") as file:
         json.dump(src, file, indent=4, ensure_ascii=False)
@@ -46,7 +83,7 @@ def get_wall_posts(group_name):
         print("Файл с ID постов найден, начинаем выборку постов")
 
     print(fresh_posts_id)
-    get_comments_post(fresh_posts_id, posts)
+    get_comments_post(fresh_posts_id, posts, group_name)
 
     #ИЗВЛЕКАЕМ ДАННЫЕ ИЗ ПОСТОВ
     #count_comments = Counter(posts["comments"])
@@ -58,14 +95,6 @@ def get_wall_posts(group_name):
          #   if "comments" in post:
         #except Exception:
             #print("Что-то пошло не так!")
-
-def get_comments_post(fresh_posts_id, posts):
-    print("GET COMMENTS POST")
-    posts_id = []
-    for item in fresh_posts_id:
-        url = f"https://api.vk.com/method/wall.getComments?domain={item}&count=10&access_token={token}&v=5.131"
-        req = requests.get(url)
-        src = req.json()
 
 
 
