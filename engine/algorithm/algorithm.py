77532158cd3449f8d4a7808e015c724e407cc577
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2022-02-13 14:32:47 +0800

    Добавлена функция получения токенов для алгоритма Добавлен счётчик букв в верхнем регистре (переменной х1)

--- engine/algorithm/algorithm.py
+++ engine/algorithm/algorithm.py
@@ -1,5 +1,8 @@
 import re
 import string
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download("")
 
 file = open(f'tests/100_sarc.txt', 'r', encoding='utf-8')
 f = " ".join(file.read().splitlines())
@@ -22,4 +25,19 @@ counts(f)
 #СЧЁТЧИК ВСЕХ СЛОВ В ТЕКСТЕ
 len_text = len(list_file)
 print("\nСчётчик всех слов в тексте:", len_text, sep="\n")
+
+#ТОКЕНИЗАЦИЯ И РАЗБИВКА НА СИМВОЛЫ
+tokens = word_tokenize(" ".join(list_file))
+print("ТОКЕНЫ: ", tokens)
+
+symbols_text = []
+for word_i in range(len(f)):
+    symbols_text.append(f[word_i])
+
+print(" ".join(symbols_text))
+#ПЕРЕМЕННАЯ Х1 - КОЛИЧЕСТВО ЗАГЛАВНЫХ БУКВ
+count_uppercase = sum(map(str.isupper, f))
+print("Количество слов в верхнем регистре (х1): ", count_uppercase)
+
+#
 
