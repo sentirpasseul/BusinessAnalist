c48754253704b5ac2e502cb3ec0c5482fa83d8b0
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2022-02-16 23:05:31 +0800

    Структурирование проекта в функции

--- engine/algorithm/algorithm.py
+++ engine/algorithm/algorithm.py
@@ -27,6 +27,7 @@ def counts(f):
 counts(f)
 """
 
+
 #СЧЁТЧИК ВСЕХ СЛОВ В ТЕКСТЕ
 len_text = len(list_file)
 print("\nСчётчик всех слов в тексте:", len_text, sep="\n")
@@ -55,48 +56,45 @@ sent1 = re.compile(r'[.|!|?|…]').split(f)
 list_sent = list(filter(None, sent1))
 len_sent = len(list_sent)
 
-#print(" ".join(symbols_text))
+    #print(" ".join(symbols_text))
+def counters(len_symb_text, list_parts, len_text, f, len_sent):
+    #ПЕРЕМЕННАЯ Х1 - КОЛИЧЕСТВО ЗАГЛАВНЫХ БУКВ
+    count_uppercase = sum(map(str.isupper, f))
+    #Вычисление эталона заглавных букв
+    x1 = round(count_uppercase/len_symb_text, 4)
+    print(f"Переменная x1 = {round(x1, 4)}")
 
-#ПЕРЕМЕННАЯ Х1 - КОЛИЧЕСТВО ЗАГЛАВНЫХ БУКВ
-count_uppercase = sum(map(str.isupper, f))
-#Вычисление эталона заглавных букв
-x1 = (count_uppercase/len_symb_text)
-print(f"Переменная x1 = {round(x1, 4)}")
-
-#ПЕРЕМЕННАЯ Х2 - КОЛИЧЕСТВО СОЮЗОВ
-count_unions = list_parts.count("СОЮЗ")
-#Вычисление эталона союзов
-x2 = (count_unions/len_text)
-print(f"Переменная х2 = {round(x2, 4)}")
+    #ПЕРЕМЕННАЯ Х2 - КОЛИЧЕСТВО СОЮЗОВ
+    count_unions = list_parts.count("СОЮЗ")
+    #Вычисление эталона союзов
+    x2 = round(count_unions/len_text, 4)
+    print(f"Переменная х2 = {round(x2, 4)}")
 
 
-#ПЕРЕМЕННАЯ Х3 - КОЛИЧЕСТВО ЧАСТИЦ
-count_particle = list_parts.count("ЧАСТ")
-#Вычисление эталона частиц
-x3 = count_particle/len_text
-print(f"Переменная х3 = {round(x3, 4)}")
+    #ПЕРЕМЕННАЯ Х3 - КОЛИЧЕСТВО ЧАСТИЦ
+    count_particle = list_parts.count("ЧАСТ")
+    #Вычисление эталона частиц
+    x3 = round(count_particle/len_text, 4)
+    print(f"Переменная х3 = {round(x3, 4)}")
 
-#ПЕРЕМЕННАЯ Х4 - КОЛИЧЕСТВО УСИЛИВАЮЩИХ ЗНАКОВ ПРЕПИНАНИЯ
-count_gain_punct = f.count("!") + f.count("?")
-#Вычисление эталона усиливающих знаков препинания
-x4 = (count_gain_punct/len_sent)/10
-print(f"Переменная х4 = {round(x4, 4)}")
+    #ПЕРЕМЕННАЯ Х4 - КОЛИЧЕСТВО УСИЛИВАЮЩИХ ЗНАКОВ ПРЕПИНАНИЯ
+    count_gain_punct = f.count("!") + f.count("?")
+    #Вычисление эталона усиливающих знаков препинания
+    x4 = round((count_gain_punct/len_sent)/10, 4)
+    print(f"Переменная х4 = {x4}")
 
-#ПЕРЕМЕННАЯ Х5 - КОЛИЧЕСТВО МЕЖДОМЕТИЙ
-count_interj = list_parts.count("МЕЖД")
-#Вычисление эталона междометий
-x5 = count_interj/len_text
-print(f"Переменная х5 = {round(x5, 4)}")
+    #ПЕРЕМЕННАЯ Х5 - КОЛИЧЕСТВО МЕЖДОМЕТИЙ
+    count_interj = list_parts.count("МЕЖД")
+    #Вычисление эталона междометий
+    x5 = round(count_interj/len_text, 4)
+    print(f"Переменная х5 = {x5}")
 
-def display_counts(count_uppercase, count_unions,
-                   count_particle, count_gain_punct,
-                    count_interj
-                   ):
-    print("\nКоличество букв в верхнем регистре (переменная х1): ", count_uppercase)
-    print("Количество союзов в тексте:", count_unions)
-    print("Количество частиц в тексте:", count_particle)
-    print("Количество усиливающих знаков препинания в тексте:", count_gain_punct)
-    print("Количество междометий в тексте:", count_interj)
+    def display_counts():
+        print("\nКоличество букв в верхнем регистре (переменная х1): ", count_uppercase)
+        print("Количество союзов в тексте:", count_unions)
+        print("Количество частиц в тексте:", count_particle)
+        print("Количество усиливающих знаков препинания в тексте:", count_gain_punct)
+        print("Количество междометий в тексте:", count_interj)
 
 """"
 standard_100 = open(f'/standards/standard_100', 'w')
@@ -105,3 +103,7 @@ with open(f'/standards/standard_100', 'w'):
                    count_particle, count_gain_punct,
                     count_interj)
 """
+def main():
+    counters(len_symb_text, list_parts, len_text, f, len_sent)
+if __name__ == '__main__':
+    main()
