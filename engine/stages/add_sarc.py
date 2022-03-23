6bad5edd072a06f3436b3129fce24dfdf16ecac2
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2022-03-23 18:38:35 +0800

    Добавлен датасет с 100% сарказмом

--- engine/stages/add_sarc.py
+++ engine/stages/add_sarc.py
@@ -1,3 +1,13 @@
+from nltk.tokenize import word_tokenize
+
+def preproc():
+    file = open('data.txt', 'r', encoding="utf-8")
+    text = file.read()
+    split_text = text.split(f"{str.isdigit}")
+    tok = word_tokenize(" ".join(split_text), "russian")
+    print(tok)
+    #print(" ".join(split_text))
+
 t = "СТОП"
 text = ""
 while t != text:
@@ -7,4 +17,4 @@ while t != text:
             if text != "СТОП":
                 file_sarc.write(text + '\n')
             else:
-                break
+                preproc()
