344e3d6ef30aec0e83d42bcc1a3ea70ad20c831f
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2022-05-08 12:59:02 +0800

    Добавлен ООП

--- engine/stages/sarcasm/main/engine_algorythm.py
+++ engine/stages/sarcasm/main/engine_algorythm.py
@@ -17,247 +17,256 @@ split_regex = re.compile(r'[|!|?|…]')
 morph = pymorphy2.MorphAnalyzer()
 nltk.downloader.Downloader('webtext')
 
-def vizual_ton(text_list: list, neg_list: list, pos_list:list, neu_list:list):
-    #positions = np.arange(len())
-    tick_labels = text_list
+class Algorythm():
+
+    def __init__(self, text):
+        self.col = "".join(collocations)
+        self.text = text
+        if text.count("\n") > 1:
+            print("Строчек больше 1")
+            self.count_sentence = text.count("\n")
+            self.split_text = text.split("\n")
+        else:
+            print("Строчек меньше двух")
+            self.count_sentence = text.count(".")
+            self.split_text = text.split(".")
+        self.neg_list, self.pos_list, self.neu_list = tonality(self.split_text)
+        print(self.split_text)
 
 
 
-def analyzer(text: str):
-    split_text = text
-    if text.count("\n") >1:
-        print("Строчек больше 1")
-        count_sentence = text.count("\n")
-        split_text = text.split("\n")
-    else:
-        print("Строчек меньше двух")
-        count_sentence = text.count(".")
-        split_text = text.split(".")
+
+    def preprocess(self):
+        text = self.text
 
 
-    print(split_text)
+        # vis = vizual_ton(split_text, neg_list, pos_list, neu_list)
+        counter = 0
 
-    neg_list, pos_list, neu_list = tonality(split_text)
-    vis = vizual_ton(split_text, neg_list, pos_list, neu_list)
-    counter = 0
-
-    znachenie_list = list()
-
-    col = "".join(collocations)
-    #w2v_model = Word2Vec(min_count=10, window=2, negative=10, alpha=0.03, min_alpha=0.0007, sample=6e-5, sg=1)
-    ##w2v_model.build_vocab(dict1)
-    #w2v_model.train(dict1, total_examples=w2v_model.corpus_count, epochs=30, report_delay=1)
-    #w2v_model.init_sims(replace=True)
-
-    #dict = {'Sentence': split_text, 'Негативно': neg_list, "Позитивно": pos_list, "Нейтрально": neu_list}
-
-    #df = pd.DataFrame(dict)
-    #print(df_width)
-
-    count_if = 0
-    sentences_ton = list()
-
-    for sentence in split_text:
-
-        #print(w2v_model.wv.most_similar(positive=[f"{sentence}"]))
+        znachenie_list = list()
 
 
-        if col in sentence:
-            count_if +=1
-            print("ЕСТь")
+    def vizual_ton(self):
 
-        #print("\n\n!!!!!!!!!!!!!!!!!!!!!!!!")
-        #print(sentence)
-        #print("!!!!!!!!!!!!!!!!!!!!!!!", end="")
+        #positions = np.arange(len())
+        s = 11
 
-        # Сплитим предложение на слова
-        sent1 = sentence.split(" ")
-        list_parts = []
 
-        #print()
-        # Разбиваем спличенный текст на токены
-        sub1_reset = word_tokenize(" ".join(sent1))
-        #if sub1_reset != " " and sub1_reset != "":
-            #print(sub1_reset)
 
-        # СЧЁТЧИК ВСЕХ СЛОВ В ТЕКСТЕ
-        len_text = len(sub1_reset)
-        #print("Счётчик всех слов в тексте:", len_text, sep="\n")
+    def analyzer(self):
+        text = self.text
+        #w2v_model = Word2Vec(min_count=10, window=2, negative=10, alpha=0.03, min_alpha=0.0007, sample=6e-5, sg=1)
+        ##w2v_model.build_vocab(dict1)
+        #w2v_model.train(dict1, total_examples=w2v_model.corpus_count, epochs=30, report_delay=1)
+        #w2v_model.init_sims(replace=True)
 
-        sum1 = 0
-        # Анализируем каждое слово в предложении
-        for word in sent1:
-            if word != "":
-                parse_parts = morph.parse(word)[0]
-                part = parse_parts.tag.cyr_repr
-                list_parts.append(part)
-                #print(f"Для слова '{word}' частью речи является '{part}'")
+        #dict = {'Sentence': split_text, 'Негативно': neg_list, "Позитивно": pos_list, "Нейтрально": neu_list}
 
-                # Сплитим строку из морф анализа
-                morph_split = "".join(part).split(",")
-                #print(morph_split)
+        #df = pd.DataFrame(dict)
+        #print(df_width)
 
-        count_bi = sentence.count("бы")
-        count_zap_no = sentence.count(','+'но')
+        count_if = 0
+        sentences_ton = list()
 
-        """
-        neg_sent, pos_sent, neu_sent = tonality(sentence)
+        for sentence in self.split_text:
 
-        sum_neg_sent = math.fsum(neg_sent)
-        sum_pos_sent = math.fsum(pos_sent)
-        sum_neu_sent = math.fsum(neu_sent)
+            #print(w2v_model.wv.most_similar(positive=[f"{sentence}"]))
+            if self.col in sentence:
+                count_if +=1
+                print("ЕСТь")
+
+            #print("\n\n!!!!!!!!!!!!!!!!!!!!!!!!")
+            #print(sentence)
+            #print("!!!!!!!!!!!!!!!!!!!!!!!", end="")
+
+            # Сплитим предложение на слова
+            sent1 = sentence.split(" ")
+            list_parts = []
+
+            #print()
+            # Разбиваем спличенный текст на токены
+            sub1_reset = word_tokenize(" ".join(sent1))
+            #if sub1_reset != " " and sub1_reset != "":
+                #print(sub1_reset)
+
+            # СЧЁТЧИК ВСЕХ СЛОВ В ТЕКСТЕ
+            len_text = len(sub1_reset)
+            #print("Счётчик всех слов в тексте:", len_text, sep="\n")
+
+            sum1 = 0
+            # Анализируем каждое слово в предложении
+            for word in sent1:
+                if word != "":
+                    parse_parts = morph.parse(word)[0]
+                    part = parse_parts.tag.cyr_repr
+                    list_parts.append(part)
+                    #print(f"Для слова '{word}' частью речи является '{part}'")
+
+                    # Сплитим строку из морф анализа
+                    morph_split = "".join(part).split(",")
+                    #print(morph_split)
+
+            count_bi = sentence.count("бы")
+            count_zap_no = sentence.count(','+'но')
+
+            """
+            neg_sent, pos_sent, neu_sent = tonality(sentence)
+    
+            sum_neg_sent = math.fsum(neg_sent)
+            sum_pos_sent = math.fsum(pos_sent)
+            sum_neu_sent = math.fsum(neu_sent)
+    
+            print()
+            print("Отрицательно:", sum_neg_sent)
+            print("Положительно:", sum_pos_sent)
+            print("Нейтрально:", sum_neu_sent)
+            print()
+            """
+            #print("Количество БЫ:", count_bi)
+            #print("Количество ', но'", count_zap_no)
+
+            """
+            form_xs = list()
+            if len_text != 0:
+    
+                # ПЕРЕМЕННАЯ Х1 - КОЛИЧЕСТВО "БЫ"
+                for s in sentence:
+                    if s.isupper():
+                        sum1 += 1
+                count_uppercase = sum1
+                # Вычисление эталона "бы"
+                x1 = count_bi
+                print(f"\nПеременная x1 ('БЫ') = {round(x1, 4)}")
+                form_xs.append(x1)
+    
+                # ПЕРЕМЕННАЯ Х2 - КОЛИЧЕСТВО СОЮЗОВ
+                count_unions = list_parts.count("СОЮЗ")
+                # Вычисление эталона союзов
+                x2 = round(count_unions / len_text, 4)
+                print(f"Переменная х2 (союзы) = {round(x2, 4)}")
+                form_xs.append(x2)
+    
+                # ПЕРЕМЕННАЯ Х3 - КОЛИЧЕСТВО ЧАСТИЦ
+                count_particle = list_parts.count("ЧАСТ")
+                # Вычисление эталона частиц
+                x3 = round(count_particle / len_text, 4)
+                print(f"Переменная х3 (частицы) = {round(x3, 4)}")
+                form_xs.append(x3)
+    
+                # ПЕРЕМЕННАЯ Х4 - КОЛИЧЕСТВО УСИЛИВАЮЩИХ ЗНАКОВ ПРЕПИНАНИЯ
+                count_gain_punct = sub1_reset.count("!") + sub1_reset.count("?")
+                # Вычисление эталона усиливающих знаков препинания
+                x4 = round((count_gain_punct / len_text) / 10, 4)
+                print(f"Переменная х4 = {x4}")
+                form_xs.append(x4)
+    
+                # ПЕРЕМЕННАЯ Х5 - КОЛИЧЕСТВО МЕЖДОМЕТИЙ
+                count_interj = list_parts.count("МЕЖД")
+                # Вычисление эталона междометий
+                x5 = round(count_interj / len_text, 4)
+                print(f"Переменная х5 (междометия) = {x5}")
+                form_xs.append(x5)
+    
+                max_x, max_x_index = max(form_xs), form_xs.index(max(form_xs)) + 1
+                min_x, min_x_index = min(form_xs), form_xs.index(min(form_xs)) + 1
+                formula = abs(math.sqrt(min_x ** 2 + max_x ** 2))
+                znachenie_list.append(formula)
+    
+            """
+            #print()
+            #print("АНАЛИЗ ТОНАЛЬНОСТИ ПРЕДЛОЖЕНИЯ:")
+            #print("###############################")
+            #print("Отрицательно:", neg_list[counter])
+            #print("Положительно:", pos_list[counter])
+            #print("Нейтрально:", neu_list[counter])
+            #print("################################")
+            #print()
+            #counter += 1
+
+            """
+            print("ВЫЧИСЛЕНИЕ ТОЧЕК МАКСИМУМА И МИНИМУМА ВХОЖДЕНИЯ В САРКАЗМ")
+            print("##########################")
+            print(znachenie_list)
+            max_sarc_list = max(znachenie_list)
+            min_sarc_list = min(znachenie_list)
+            print("Максимальное значение сарказма", "(x" + f"{max_x_index}" + "):", max_sarc_list, )
+            print("Минимальное значение сарказма", "(x" + f"{min_x_index}" + "):", min_sarc_list)
+            print("##########################")
+            """
+        for sentence in zip(self.split_text, self.neg_list, self.neu_list, self.pos_list):
+            df1 = pd.DataFrame(columns=["sentences", "Негативно", "Позитивно", "Нейтрально"])
+        df = pd.DataFrame(sentences_ton)
+        df_width = df.style.set_properties(subset=['Sentence'], **{'width': '300px'})
+
+        print("ТЕСТИРОВАНИЕ ФУНКЦИИ")
+        print(df)
+
+        sum_neg = math.fsum(self.neg_list)
+        sum_neu = math.fsum(self.neu_list)
+        sum_pos = math.fsum(self.pos_list)
+
+        if sum_pos != 0 or sum_neu != 0 or sum_neg != 0:
+            diff_neg = sum_neg / self.count_sentence
+            diff_neu = sum_neu / self.count_sentence
+            diff_pos = sum_pos / self.count_sentence
+
+            print()
+            print("Вычисление меры негатива на одно предложение:", diff_neg)
+            print("Вычисление меры позитива на одно предложение:", diff_pos)
+            print("Вычисление меры нейтральности на одно предложение:", diff_neu)
+            print()
 
         print()
-        print("Отрицательно:", sum_neg_sent)
-        print("Положительно:", sum_pos_sent)
-        print("Нейтрально:", sum_neu_sent)
+        print("#########################")
+        print("ОБЩАЯ ОЦЕНКА ТОНАЛЬНОСТИ ТЕКСТА")
+        print("Отрицательно:", sum_neg)
+        print("Положительно:", sum_neu)
+        print("Нейтрально:", sum_pos)
+
+
+
+        print("СЧЁТЧИК ВСЕХ ПРЕДЛОЖЕНИЙ В ТЕКСТЕ")
+        print(self.count_sentence)
+
+            # Часто встречающиеся слова
+
+        # Частота "не" в предложении
+
+        #from nltk.corpus import stopwords
+        #ru_stopwords = stopwords.words('russian')
+        # tokens = word_tokenize(sentence, language='russian')
+        filtered_tokens = []
+        #for sentence in split_text:
+            #for token in sentence:
+                #if token not in ru_stopwords:
+                #    filtered_tokens.append(token)
+        words = " ".join(self.split_text).split(" ")
+        count_q = words.count('"')
+        if count_q > 0:
+            count_q = True
+
+        for word in words:
+            filtered_tokens.append(word)
+
+        fdist = FreqDist(filtered_tokens)
         print()
-        """
-        #print("Количество БЫ:", count_bi)
-        #print("Количество ', но'", count_zap_no)
-
-        """
-        form_xs = list()
-        if len_text != 0:
-
-            # ПЕРЕМЕННАЯ Х1 - КОЛИЧЕСТВО "БЫ"
-            for s in sentence:
-                if s.isupper():
-                    sum1 += 1
-            count_uppercase = sum1
-            # Вычисление эталона "бы"
-            x1 = count_bi
-            print(f"\nПеременная x1 ('БЫ') = {round(x1, 4)}")
-            form_xs.append(x1)
-
-            # ПЕРЕМЕННАЯ Х2 - КОЛИЧЕСТВО СОЮЗОВ
-            count_unions = list_parts.count("СОЮЗ")
-            # Вычисление эталона союзов
-            x2 = round(count_unions / len_text, 4)
-            print(f"Переменная х2 (союзы) = {round(x2, 4)}")
-            form_xs.append(x2)
-
-            # ПЕРЕМЕННАЯ Х3 - КОЛИЧЕСТВО ЧАСТИЦ
-            count_particle = list_parts.count("ЧАСТ")
-            # Вычисление эталона частиц
-            x3 = round(count_particle / len_text, 4)
-            print(f"Переменная х3 (частицы) = {round(x3, 4)}")
-            form_xs.append(x3)
-
-            # ПЕРЕМЕННАЯ Х4 - КОЛИЧЕСТВО УСИЛИВАЮЩИХ ЗНАКОВ ПРЕПИНАНИЯ
-            count_gain_punct = sub1_reset.count("!") + sub1_reset.count("?")
-            # Вычисление эталона усиливающих знаков препинания
-            x4 = round((count_gain_punct / len_text) / 10, 4)
-            print(f"Переменная х4 = {x4}")
-            form_xs.append(x4)
-
-            # ПЕРЕМЕННАЯ Х5 - КОЛИЧЕСТВО МЕЖДОМЕТИЙ
-            count_interj = list_parts.count("МЕЖД")
-            # Вычисление эталона междометий
-            x5 = round(count_interj / len_text, 4)
-            print(f"Переменная х5 (междометия) = {x5}")
-            form_xs.append(x5)
-
-            max_x, max_x_index = max(form_xs), form_xs.index(max(form_xs)) + 1
-            min_x, min_x_index = min(form_xs), form_xs.index(min(form_xs)) + 1
-            formula = abs(math.sqrt(min_x ** 2 + max_x ** 2))
-            znachenie_list.append(formula)
-
-        """
-        #print()
-        #print("АНАЛИЗ ТОНАЛЬНОСТИ ПРЕДЛОЖЕНИЯ:")
-        #print("###############################")
-        #print("Отрицательно:", neg_list[counter])
-        #print("Положительно:", pos_list[counter])
-        #print("Нейтрально:", neu_list[counter])
-        #print("################################")
-        #print()
-        #counter += 1
-
-        """
-        print("ВЫЧИСЛЕНИЕ ТОЧЕК МАКСИМУМА И МИНИМУМА ВХОЖДЕНИЯ В САРКАЗМ")
-        print("##########################")
-        print(znachenie_list)
-        max_sarc_list = max(znachenie_list)
-        min_sarc_list = min(znachenie_list)
-        print("Максимальное значение сарказма", "(x" + f"{max_x_index}" + "):", max_sarc_list, )
-        print("Минимальное значение сарказма", "(x" + f"{min_x_index}" + "):", min_sarc_list)
-        print("##########################")
-        """
-    for sentence in zip(split_text, neg_list, neu_list, pos_list):
-        df1 = pd.DataFrame(columns=["sentences", "Негативно", "Позитивно", "Нейтрально"])
-    df = pd.DataFrame(sentences_ton)
-    df_width = df.style.set_properties(subset=['Sentence'], **{'width': '300px'})
-
-    print("ТЕСТИРОВАНИЕ ФУНКЦИИ")
-    print(df)
-
-    sum_neg = math.fsum(neg_list)
-    sum_neu = math.fsum(neu_list)
-    sum_pos = math.fsum(pos_list)
-
-    if sum_pos != 0 or sum_neu != 0 or sum_neg != 0:
-        diff_neg = sum_neg / count_sentence
-        diff_neu = sum_neu / count_sentence
-        diff_pos = sum_pos / count_sentence
+        print("ЧАСТОТА СЛОВ")
+        print("########################")
+        #print(*fdist.most_common(len(filtered_tokens)), sep='\n')
+        print("########################")
+        print()
+        #fdist.plot(15)
 
         print()
-        print("Вычисление меры негатива на одно предложение:", diff_neg)
-        print("Вычисление меры позитива на одно предложение:", diff_pos)
-        print("Вычисление меры нейтральности на одно предложение:", diff_neu)
+        print("СЧЁТЧИК ЧАСТИЦ 'НЕ, ВОТ, ЕСЛИ'")
+        print("#############################")
+        print('Количество "Не" в предложенни:', words.count("не"))
+        print('Количество "Вот" в предложенни:', words.count("вот"))
+        print('Количество "Если" в предложенни:', words.count("если"))
+        print('Количество ", eсли" в предложенни:', count_if)
+        print('Количество "Бы" в предложении:', words.count("бы"))
+        print('Количество ", но" в предложении:', words.count(", но"))
+        print("#############################")
         print()
-
-    print()
-    print("#########################")
-    print("ОБЩАЯ ОЦЕНКА ТОНАЛЬНОСТИ ТЕКСТА")
-    print("Отрицательно:", sum_neg)
-    print("Положительно:", sum_neu)
-    print("Нейтрально:", sum_pos)
-
-
-
-    print("СЧЁТЧИК ВСЕХ ПРЕДЛОЖЕНИЙ В ТЕКСТЕ")
-    print(count_sentence)
-
-        # Часто встречающиеся слова
-
-    # Частота "не" в предложении
-
-    #from nltk.corpus import stopwords
-    #ru_stopwords = stopwords.words('russian')
-    # tokens = word_tokenize(sentence, language='russian')
-    filtered_tokens = []
-    #for sentence in split_text:
-        #for token in sentence:
-            #if token not in ru_stopwords:
-            #    filtered_tokens.append(token)
-    words = " ".join(split_text).split(" ")
-    count_q = words.count('"')
-    if count_q > 0:
-        count_q = True
-
-    for word in words:
-        filtered_tokens.append(word)
-
-    fdist = FreqDist(filtered_tokens)
-    print()
-    print("ЧАСТОТА СЛОВ")
-    print("########################")
-    #print(*fdist.most_common(len(filtered_tokens)), sep='\n')
-    print("########################")
-    print()
-    #fdist.plot(15)
-
-    print()
-    print("СЧЁТЧИК ЧАСТИЦ 'НЕ, ВОТ, ЕСЛИ'")
-    print("#############################")
-    print('Количество "Не" в предложенни:', words.count("не"))
-    print('Количество "Вот" в предложенни:', words.count("вот"))
-    print('Количество "Если" в предложенни:', words.count("если"))
-    print('Количество ", eсли" в предложенни:', count_if)
-    print('Количество "Бы" в предложении:', words.count("бы"))
-    print('Количество ", но" в предложении:', words.count(", но"))
-    print("#############################")
-    print()
 
     #print("Count Quotes:", count_q)
--- engine/stages/sarcasm/main/etalon.py
+++ engine/stages/sarcasm/main/etalon.py
@@ -11,17 +11,17 @@ def algorythm():
     ds_nsarc2 = f_nsarc2
 
     #print("ТЕКСТ С САРКАЗМОМ")
-    process_sarc1 = analyzer(ds_sarc)
+    process_sarc1 = Algorythm(ds_sarc).analyzer()
     #print("ТЕКСТ БЕЗ САРКАЗМА")
-    process_nsarc1 = analyzer(ds_nsarc)
+    process_nsarc1 = Algorythm(ds_nsarc).analyzer()
 
     print("Текст сарказма из Википедии")
     print()
-    process_wiki_sarc = analyzer(ds_wiki_sarc)
+    process_wiki_sarc = Algorythm(ds_wiki_sarc).analyzer()
 
     print("Текст без сарказма")
     print()
-    p_nsarc2 = analyzer(ds_nsarc2)
+    p_nsarc2 = Algorythm(ds_nsarc2).analyzer()
 
     return process_sarc1, process_nsarc1, process_wiki_sarc, p_nsarc2
 
